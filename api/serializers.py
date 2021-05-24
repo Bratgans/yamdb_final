00ca43683ecd9214826f2b23bc8b44533ee39baf
Bratgans <bratgans@gmail.com>
2021-05-24 14:40:06 +0300

    serializer test_2

--- api/serializers.py
+++ api/serializers.py
@@ -31,10 +31,9 @@ class UserCreateSerializer(serializers.Serializer):
         email = validated_data.get('email')
         try:
             user = User.objects.get(email=email)
-            return user
         except Exception:
             raise serializers.ValidationError({'email': ['invalid email']})
-
+        return user
 
 
 class GenreSerializer(serializers.ModelSerializer):
